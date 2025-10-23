dccef781e7065369ce2603de55a69b8cda1fd9d1
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-10-23 11:06:38 +0200

    Add navigation items to Layout component and create SCSS partials for header, footer, product, mixins, style, and variables

--- js/controllers/layoutController.js
+++ js/controllers/layoutController.js
@@ -5,7 +5,11 @@ export const Layout = (title, content) => {
   document.title = title;
 
   const arrNavItems = [
+    { href: "/index.htm#/forside", title: "Forside" },
     { href: "/index.htm#/produkter", title: "Produkter" },
+    { href: "/index.htm#/forhandler", title: "Forhandler" },
+    { href: "/index.htm#/omos", title: "Om os" },
+    { href: "/index.htm#/kontakt", title: "Kontakt" },
     { href: "/index.htm#/cart", title: "Indkøbskurv" },
   ];
 
