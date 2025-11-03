1fdb626553f04816b40297fb9fb97aad704a7511
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-11-03 12:08:30 +0100

    metal15

--- js/controllers/cartController.js
+++ js/controllers/cartController.js
@@ -1,9 +1,25 @@
 import { getCartList } from "../models/cartModel.js";
-import { cartListView } from "../views/organisms/cartView.js";
+import { cartListHeaderView, cartListView } from "../views/organisms/cartView.js";
 import { Layout } from "../controllers/layoutController.js";
 
 export const Cartpage = async () => {
   const data = await getCartList();
-  const html = cartListView(data);
+
+  const arrHeaderColumns = [
+    { name: "Antal", className: "w-[10%] font-bold" },
+    { name: "Produkt", className: "w-[60%] font-bold" },
+    { name: "Pris", className: "w-[20%] font-bold" },
+    { name: "Handling", className: "w-[10%] font-bold" },
+  ];
+
+  const totalPrice = data.reduce((sum, item) => {
+    return sum + (item?.product?.price * item?.quantity || 0);
+  }, 0);
+
+  const html = Div();
+  html.append(cartListHeaderView(arrHeaderColumns));
+  html.append(cartListView(data));
+  html.append(cartTotalView(totalPrice));
+
   return Layout("Indkøbskurv", html);
 };
